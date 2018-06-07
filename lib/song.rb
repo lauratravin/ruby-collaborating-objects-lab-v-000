b4cc4b1e17de8fda77b6c4355fa4c4579f430ba8
Laura Messa Travin <laura.travin@gmail.com>
2018-06-07 04:08:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,8 @@ class Song
 
     def self.new_by_filename(file_name)
         songname = file_name.split(" - ")[1]
-        artist = Artist.find_or_create_by_name(file_name.split(" - ")[0],songname)
-    #    artist.add_song(songname)
+        artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
+        artist.add_song(songname)
         Song.new(songname,artist)
 
     end
